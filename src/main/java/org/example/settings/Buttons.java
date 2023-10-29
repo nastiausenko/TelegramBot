5f2b9c8126c18968f5eca78bef236b3828bd38fe
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2023-10-29 21:34:12 +0200

    put the message text into constants

--- src/main/java/org/example/settings/Buttons.java
+++ src/main/java/org/example/settings/Buttons.java
@@ -10,9 +10,16 @@ import java.util.List;
 import java.util.Map;
 
 public class Buttons {
+    private static final String WELCOME_MESSAGE = "Ласкаво просимо. Цей бот допопможе відслідковувати актуальні курси валют";
+    private static final String SETTINGS = "Налаштування";
+    private static final String DECIMAL_PLACES_MESSAGE = "Виберіть кількість знаків після коми";
+    private static final String BANK_MESSAGE = "Виберіть банк";
+    private static final String CURRENCY_MESSAGE = "Виберіть валюту";
+
+
 
     public void startButtons(SendMessage message) {
-        message.setText("Ласкаво просимо. Цей бот допопможе відслідковувати актуальні курси валют");
+        message.setText(WELCOME_MESSAGE);
         attachButtons(message, Map.of(
                 "Отримати інфо\n", "get_info",
                 "Налаштування\n", "settings"
@@ -20,7 +27,7 @@ public class Buttons {
     }
 
     public void settingsButtons(SendMessage message) {
-        message.setText("Налаштування");
+        message.setText(SETTINGS);
         attachButtons(message, Map.of(
                 "Кількість знаків після коми\n", "decimal_places",
                 "Банк\n", "bank",
@@ -30,7 +37,7 @@ public class Buttons {
     }
 
     public void decimalPlacesButtons(SendMessage message) {
-        message.setText("Виберіть кількість знаків після коми");
+        message.setText(DECIMAL_PLACES_MESSAGE);
         attachButtons(message, Map.of(
                 "2\n", "2",
                 "3\n", "3",
@@ -39,7 +46,7 @@ public class Buttons {
     }
 
     public void bankButtons(SendMessage message) {
-        message.setText("Виберіть банк");
+        message.setText(BANK_MESSAGE);
         attachButtons(message, Map.of(
                 "НБУ\n", "nbu",
                 "ПриватБанк\n", "privatbank",
@@ -48,7 +55,7 @@ public class Buttons {
     }
 
     public void currencyButtons(SendMessage message) {
-        message.setText("Виберіть валюту");
+        message.setText(CURRENCY_MESSAGE);
         attachButtons(message, Map.of(
                 "EUR\n", "eur",
                 "USD\n", "usd"
